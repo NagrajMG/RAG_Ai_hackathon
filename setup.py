3010d08b37d3991f987b38a660416510a0f6c664
Nagraj Gaonkar <nagrajgaonkarmumbai749@gmail.com>
2024-10-21 17:28:52 +0530

    Second commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+name='RAG',
+
+version='0.0.1',
+
+author= 'Nagraj_Gaonkar',
+
+author_email='nagrajgaonkarmumbai749@gmail.com',
+
+install_requires=['langchain_groq'],
+
+packages=find_packages()
+)
